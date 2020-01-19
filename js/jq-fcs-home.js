216b2e1b66520b99131b3690bfcd5054c8c8c536
doiac <59356052+doiac@users.noreply.github.com>
2020-01-19 19:17:43 +0800

    Add files via upload

--- js/jq-fcs-home.js
+++ js/jq-fcs-home.js
@@ -3,14 +3,34 @@ $(document).ready(function(){
     $('#map').css('width',600);
   else
     $('#map').css('width',$(window).width()-250);
-  for(let i = 0;i < 3; i++){
-    $('.home-content-'+i).css('padding-left',100+'px');
-    $('.home-content-'+i).css('padding-right',100+'px');
+  if($(window).width()>375){
+    for(let i = 0;i < 3; i++){
+      $('.home-content-'+i).css('padding-left',100+'px');
+      $('.home-content-'+i).css('padding-right',100+'px');
+    }
+  }
+  else {
+    for(let i = 0;i < 3; i++){
+      $('.home-content-'+i).css('padding-left',50+'px');
+      $('.home-content-'+i).css('padding-right',50+'px');
+    }
   }
   $(window).resize(function(){
     if($(window).width()>800)
       $('#map').css('width',600);
     else
       $('#map').css('width',$(window).width()-250);
+    if($(window).width()>375){
+      for(let i = 0;i < 3; i++){
+        $('.home-content-'+i).css('padding-left',100+'px');
+        $('.home-content-'+i).css('padding-right',100+'px');
+      }
+    }
+    else {
+      for(let i = 0;i < 3; i++){
+        $('.home-content-'+i).css('padding-left',50+'px');
+        $('.home-content-'+i).css('padding-right',50+'px');
+      }
+    }
   });
 });

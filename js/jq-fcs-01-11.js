f454b4f7b65d8bace6c56d261afaab3a8eb32ca0
doiac <59356052+doiac@users.noreply.github.com>
2020-01-22 04:32:44 +0800

    Add files via upload

--- js/jq-fcs-01-11.js
+++ js/jq-fcs-01-11.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  reset_summary();
   $('#0').css('color','#4D1F00');
   $('#1').css('color','#4D1F00');
   $(window).resize(function(){
@@ -13,23 +12,21 @@ function reset_summary() {
     $('#summary-title-'+i).css('height','auto');
     $('#logo-'+i).css('width',$('#summary-'+i).width()*0.4);
     $('#logo-'+i+' img').attr('width',$('#summary-'+i).width()*0.4);
-    $('#summary-title-'+i).css('width',$('#summary-'+i).width()*0.6-5);
-    if($('#logo-'+i).height()>=$('#logo-'+i+' img').height()){
-      $('#logo-'+i).css('height',$('#logo-'+i+' img').height());
-    }
+    $('#summary-title-'+i).css('width',$('#summary-'+i).width()*0.6-5+'px');
+    $('#logo-'+i).css('height',$('#logo-'+i+' img').height());
+    // if($(window).width()<1070)
+    //   $('#summary-title-'+i).css('line-height','normal');
+    // else if($('#summary-title-'+i).height()<$('#logo-'+i).height())
+    //   $('#summary-title-'+i).css('line-height',$('#summary-title-'+i).height()/2+'px');
+  }
+  aligned();
+}
+
+function aligned(){
+  for(let i = 0; i < 5;i++){
     if($('#logo-'+i).height()>=$('#summary-title-'+i).height())
       $('#summary-title-'+i).css('height',$('#logo-'+i).height());
     else
       $('#logo-'+i).css('height',$('#summary-title-'+i).height());
-    if($(window).width()<1070){
-      for(let i = 0;i < 5;i++)
-        $('#summary-title-'+i).css('line-height','normal');
-    }
-    else
-      $('#summary-title-'+i).css('line-height',($('#summary-title-'+i).height()/2)+'px');
-    if($('#logo-'+i).height()>=$('#summary-title-'+i).height())
-      $('#summary-title-'+i).css('height',$('#logo-'+i).height());
-    else
-      $('#logo-'+i).css('height',$('#summary-title-'+i).height())
   }
 }

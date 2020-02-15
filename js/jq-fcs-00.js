601034851ec735141bab01c9b191b7b3812979fa
doiac <59356052+doiac@users.noreply.github.com>
2020-02-15 20:55:55 +0800

    Add files via upload

--- js/jq-fcs-00.js
+++ js/jq-fcs-00.js
@@ -1,3 +1,4 @@
+var urls = ['1I7fdd8A7JGud7YkFsMkYw5ENeryJvuXB','1dgdV8uqOWGfsJo3EHNvbYubdhBqwOEn7','1848cLDvAvCFU7yK_OFe1rMvmUM1rJB15','1YoMfEzoZaleuTId05xUDDbmByKYNi3D0','1nL5DOHR6U5HeCTQ_srArrXBEGocsZdUE']
 $(document).ready(function(){
   $('#0').css('color','#28262C');
   $('#menu-0-option0').css('color','#28262C');
@@ -5,7 +6,13 @@ $(document).ready(function(){
     // console.log($(this)[0].id);
     $(this).click(function(){
       $('#poster').hide();
-      $('#pdf-viewer').attr('src','http://docs.google.com/gview?url=https://doiac.github.io/files/AMD'+ $(this)[0].id +'.pdf&embedded=true');
+      $('p span').each(function(){
+        $(this).css('color','inherit');
+        $(this).css('text-decoration-style','dotted');
+      });
+      $(this).css('color','#28262C');
+      $(this).css('text-decoration-style','solid');
+      $('#pdf-viewer').attr('src','https://drive.google.com/file/d/'+ urls[$(this)[0].id] +'/preview');
       $('#pdf-viewer').attr('height',$(window).height()*0.8+'px');
       if($('#pdf-viewer').css('display') == 'none')
         $('#pdf-viewer').css('display','block');

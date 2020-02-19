80d06ecab2a26801df7dc204cafb346208d3004c
doiac <59356052+doiac@users.noreply.github.com>
2020-02-20 03:08:18 +0800

    Add files via upload

---
+++ jq-fcs-02.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('#0').css('color','#28262C');
+  $('#menu-0-option2').css('color','#28262C');
+  align_poster();
+  lecture_seperate();
+  $(window).resize(function(){
+    align_poster();
+  });
+});
+
+function lecture_seperate(){
+  var count = 0;
+  $('.lecture').each(function(){
+    if(count%2)
+      $(this).css('background-color','rgba(191,192,192,0.9)');
+    count+=1;
+  });
+}
+
+function align_poster(){
+  var min = 0;
+  $('.img-container a img').each(function(){
+    if($(this).height()>min)min = $(this).height();
+  });
+  $('.poster-img').css('min-height',min+'px');
+}

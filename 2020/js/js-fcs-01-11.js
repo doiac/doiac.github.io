763fc0b1a180cba0f81e5c264fb5b84e010a3612
doiac <59356052+doiac@users.noreply.github.com>
2020-11-19 18:30:23 +0800

    add folder

---
+++ 2020/js/js-fcs-01-11.js
@@ -0,0 +1,1 @@
+window.addEventListener('load',reset_summary,false);
---
+++ 2020/js/js-fcs-10.js
@@ -0,0 +1,23 @@
+function printlist(){
+  check_footer();
+  var button = [];
+  for(let i = 0 ; i < 8; i++)
+    button[i] = document.getElementById('ic-'+i);
+  for(let i = 0 ; i < 8; i++){
+    button[i].addEventListener('click',function(){
+      clear();
+      document.getElementById('ic-'+i).style.color = '#28262C';
+      document.getElementById('c-'+i).style.display = 'block';
+      check_footer();
+    },false);
+  }
+}
+
+function clear(){
+  for(let i = 0 ; i < 8; i++){
+    document.getElementById('ic-'+i).style.color = 'inherit';
+    document.getElementById('c-'+i).style.display = 'none';
+  }
+}
+
+window.addEventListener('load',printlist,false);

4069f12b5b27b0da57984ff1dbabae0ddf65d3d3
doiac <59356052+doiac@users.noreply.github.com>
2021-01-31 22:46:13 +0800

    Update

--- 2021/js/main_jq.js
+++ 2021/js/main_jq.js
@@ -3,22 +3,26 @@ $(document).ready(function(){
   $('.title_text').css('width',$('#title_nav_0').width()*(1-$('.title_img').width()/$('#title_nav_0').width()-0.05)+'px');
 
   $('.menu_4').hover(function(){
+    // console.log('menu_4');
     $(this).find('#arrow').hover(function(){
+      // console.log('arrow');
       $('ul').show();
       $('#arrow').attr('src','images/Icon/arrow.svg');
       $('#enterprise_arrow').attr('src','../images/Icon/arrow.svg');
-      $('.menu_4_text a').css('color','white');
+      // $('.menu_4_text a').css('color','white');
       $('.menu_4_text a').css('text-shadow','-1px 1px 0 #FCA619, 1px 1px 0 #FCA619, 1px -1px 0 #FCA619, -1px -1px 0 #FCA619');
     });
     $(this).find('a').hover(function(){
+      // console.log('a');
       $('ul').show();
       $('#arrow').attr('src','images/Icon/arrow.svg');
       $('#enterprise_arrow').attr('src','../images/Icon/arrow.svg');
-      $('.menu_4_text a').css('color','white');
+      // $('.menu_4_text a').css('color','white');
       $('.menu_4_text a').css('text-shadow','-1px 1px 0 #FCA619, 1px 1px 0 #FCA619, 1px -1px 0 #FCA619, -1px -1px 0 #FCA619');
     });
   },
     function(){
+      // console.log('menu_4 exit');
       $('ul').hide();
       $('#arrow').attr('src','images/Icon/arrow_origin.svg');
       $('#enterprise_arrow').attr('src','../images/Icon/arrow_origin.svg');

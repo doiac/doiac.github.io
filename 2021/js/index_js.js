93c4d32d069a9e9977094b9ddcedd636b9b13a9f
doiac <59356052+doiac@users.noreply.github.com>
2021-03-11 11:18:45 +0800

    Update index_js.js

--- 2021/js/index_js.js
+++ 2021/js/index_js.js
@@ -94,7 +94,7 @@ $(document).ready(function(e) {
 
   if(Cookies.get('FIRST_RELOAD') === Cookies.get('nothing')){
     Cookies.set('FIRST_RELOAD', '1');
-    window.location.replace(window.location.href);
+    location.reload(false);
   }
   else
     Cookies.remove('FIRST_RELOAD');
